42d2ebda57b8c07d7ae2944151de3c1b0da03960
Yan Gabriel <yangabrielogy@gmail.com>
2022-07-18 18:30:48 -0300

    Fix: algumas docstrings que faltavam

--- restore.py
+++ restore.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 class BGX:
 
-    base_files = ["application", "core", "plugins"]
+    base_files = ["application", "core", "plugins"]  # files that you always have in bgx directory.
 
     config_name = "settings_modern.dat"
     config_dir = f"backup/{config_name}"
@@ -21,6 +21,7 @@ class BGX:
 
     def start(self) -> str:
         if not self.__check_settings():
+            # if the configuration to be installed was not found.
             return "Add your BGX configuration to the backup folder in this directory."
 
         bgx_folder = self.__get_bgx_folder()
@@ -30,6 +31,7 @@ class BGX:
             return bgx_folder["message"]
 
         if not self.__check_bgx_dir(bgx_folder["path"]):
+            # just a warning, that some essential bgx folder has not been identified.
             print("One of the essential folders are not in the BGX directory.")
 
         bgx_folder = bgx_folder["path"]
@@ -39,6 +41,7 @@ class BGX:
             shutil_copy(self.config_dir, bgx_folder)
             shutil_copy(config_to_backup, self.old_config_dir)
         except FileNotFoundError:
+            # if an old configuration was not found.
             return "Your new settings have been installed."
         else:
             return "Your new settings have been installed and the old ones have been saved."
@@ -71,6 +74,8 @@ class BGX:
         }
 
     def __get_correct_folder_path(self, possible_folders: list) -> str:
+        # basically, this function takes the folder that was most recently edited.
+
         objects = {}
 
         for folder in possible_folders:
